31929e1ad5f0025192f10aa1ad1819d22513324a
VladimirKorolev <theing1986@gmail.com>
2026-09-19 21:20:04 +0300

    fix: rename DB_* env vars to avoid clash with airflow entrypoint

--- scripts/load_raw.py
+++ scripts/load_raw.py
@@ -5,11 +5,11 @@ import os
 
 load_dotenv() 
 
-db_user = os.getenv('DB_USER')
-db_password = os.getenv('DB_PASSWORD')
-db_name = os.getenv('DB_NAME')
-db_port = os.getenv('DB_PORT')
-db_host = os.getenv('DB_HOST')
+db_user = os.getenv('SALES_DB_USER')
+db_password = os.getenv('SALES_DB_PASSWORD')
+db_name = os.getenv('SALES_DB_NAME')
+db_port = os.getenv('SALES_DB_PORT')
+db_host = os.getenv('SALES_DB_HOST')
 
 DATABASE_URL=f"postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
 
